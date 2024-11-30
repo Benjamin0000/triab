98682aae06bc63095ba8e1c6bf07278e218f031f
Home <you@example.com>
2024-11-30 15:08:11 +0100

    made a change

--- resources/views/front/includes/header.blade.php
+++ resources/views/front/includes/header.blade.php
@@ -4,7 +4,7 @@
 		<!-- Required meta tags -->
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<meta name="theme-color" content="#190f3c" />
+		<meta name="theme-color" content="#25245e" />
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="/assets/frontpage/css/bootstrap.min.css">
 		<!-- Owl Theme Default CSS -->
